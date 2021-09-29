2fef8c0ab85574597f85e272379437dfa89e1edc
Ayaka Tsubouchi <ayakawell@gmail.com>
2021-09-28 17:50:22 -0700

    first commit

---
+++ frontity.settings.js
@@ -0,0 +1,29 @@
+const settings = {
+  "name": "hello-frontity",
+  "state": {
+    "frontity": {
+      "url": "http://localhost:8888",
+      // "url": "https://test.frontity.org",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+        name: "my-first-theme"
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "url": "http://localhost:8888"
+          // "url": "https://test.frontity.org"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
---
+++ packages/my-first-theme/src/components/index.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { connect, Global, css , styled } from "frontity"
+import Link from "@frontity/components/link";
+import Switch from "@frontity/components/switch";
+import List from "./list";
+import Post from "./post";
+import Page from "./page"
+
+const Root = ({ state }) => {
+  const data = state.source.get(state.router.link);
+  return (
+    <>
+          <Global
+        styles={css`
+          html {
+            font-family: system-ui, Verdana, Arial, sans-serif;
+            a{
+                color:black;
+            }
+          }
+        `}
+      />   <Header>
+      <h1>Frontity Workshop</h1>
+      <p>Current URL: {state.router.link}</p>
+      <nav>
+        <Link link="/">Home</Link>
+        <br />
+        <Link link="/page/2">More posts</Link>
+        <br />
+        <Link link="/about-us">About Us</Link>
+      </nav>
+      </Header>
+      <main>
+        <Switch>
+          <List when={data.isArchive}>This is a list</List>
+          <Post when={data.isPost} />
+          <Page when={data.isPage} />
+        </Switch>
+      </main>
+    </>
+  );
+};
+
+export default connect(Root);
+const Header = styled.header`
+   background-color: #e5edee;
+  border-width: 0 0 8px 0;
+  border-style: solid;
+  border-color: maroon;
+
+  h1 {
+    color: #4a4a4a;
+  }
+`
