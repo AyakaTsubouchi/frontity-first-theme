d8af405cec7d0eadc535a62ba2bf8e3478002a02
Ayaka Tsubouchi <ayakawell@gmail.com>
2021-09-28 20:50:20 -0700

    change the sauce

--- frontity.settings.js
+++ frontity.settings.js
@@ -2,8 +2,8 @@ const settings = {
   "name": "hello-frontity",
   "state": {
     "frontity": {
-      "url": "http://localhost:8888",
-      // "url": "https://test.frontity.org",
+      // "url": "http://localhost:8888",
+      "url": "https://test.frontity.org",
       "title": "Test Frontity Blog",
       "description": "WordPress installation for Frontity development"
     }
@@ -16,8 +16,8 @@ const settings = {
       "name": "@frontity/wp-source",
       "state": {
         "source": {
-          "url": "http://localhost:8888"
-          // "url": "https://test.frontity.org"
+          // "url": "http://localhost:8888"
+          "url": "https://test.frontity.org"
         }
       }
     },
